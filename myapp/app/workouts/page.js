515b035f43f2978068abe2cb68f405539167cb37
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-12-31 23:38:40 -0500

    update

--- myapp/app/workouts/page.js
+++ myapp/app/workouts/page.js
@@ -1,6 +1,7 @@
 import WorkoutForm from '@/components/Form/WorkoutForm'
 import Modal from '@/components/modal/Modal'
 import HeadingText from '@/components/ui/HeadingText'
+import Link from 'next/link'
 import React from 'react'
 import { FiActivity, FiPlusCircle } from 'react-icons/fi'
 
@@ -26,6 +27,66 @@ const WorkoutsPage = () => {
         children={<WorkoutForm />}
         />
         </div>
+        </div>
+        <div className='mt-8 container max-w-3xl px-4 mx-auto sm:px-8'>
+          <div className='px-4 py-4 -mx-4 overflow-x-auto sm:-mx-8 sm:px-8 bg-gray-100 rounded-md'>
+            <div className='inline-block min-w-full overflow-hidden rounded-md'>
+              <table className='min-w-full leading-normal'>
+                <thead>
+                  <tr>
+                    <th
+                    className='px-5 py-3 text-sm font-normal text-center text-gray-800 uppercase bg-white border-b border-gray-200'
+                    >
+                      User
+                    </th>
+                    <th
+                    className='px-5 py-3 text-sm font-normal text-center text-gray-800 uppercase bg-white border-b border-gray-200'
+                    >
+                      Name
+                    </th>
+                    <th
+                    className='px-5 py-3 text-sm font-normal text-center text-gray-800 uppercase bg-white border-b border-gray-200'
+                    >
+                      Details
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td
+                    className='px-5 py-5 text-sm bg-white border-b border-gray-200'
+                    >
+                      <div className='flex items-center'>
+                        <div className='flex-shrink-0'>
+                          <Link href={'/'}>
+                            <img
+                              src='mx-auto object-cover rounded-full h-10 w-10'
+                              alt='creator'
+                            />
+                          </Link>
+                        </div>
+                        <div className='ml-3'>
+                          <p
+                          className='text-gray-900 whitespace-no-wrap'
+                          >Creator</p>
+                        </div>
+                      </div>
+                    </td>
+                    <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                <p class="text-gray-900 whitespace-no-wrap">
+                                    Admin
+                                </p>
+                            </td>
+                            <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                <p class="text-gray-900 whitespace-no-wrap">
+                                    12/09/2020
+                                </p>
+                            </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
     </section>
   )
---
+++ myapp/lib/data.js
@@ -0,0 +1,26 @@
+'use server'
+import connectDB from "./database"
+import WorkoutModel from "@/models/workoutModel"
+
+export const getWorkouts = async () => {
+    try {
+        connectDB()
+        const workouts = await WorkoutModel.find({})
+        return workouts
+    } catch (error) {
+        throw new Error("Failed to fetch workouts! " + error);
+
+    }
+}
+
+export const getPost = async (id) => {
+try {
+    connectDB()
+    const post = await WorkoutModel.findById(id)
+    return post
+} catch (error) {
+    throw new Error("Failed to fetch workout! " + error);
+
+}
+
+}
